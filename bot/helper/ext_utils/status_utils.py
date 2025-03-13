3a402a1938695aedafaca7229ec3bc59097e0a01
tanmayf <fndmydvc0077@gmail.com>
2025-03-13 23:52:11 +0530

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -191,8 +191,8 @@ def get_progress_bar_string(pct):
         100
     )
     cFull = int(p // 10)
-    p_str = "✧" * cFull
-    p_str += "✦" * (10 - cFull)
+    p_str = "🌝" * cFull
+    p_str += "🌚" * (10 - cFull)
     return f"{p_str}"
 
 

889f5479dd65dbfe4c016c196d5c1c895c741d44
tanmayf <freestff007700@gmail.com>
2025-03-21 21:37:44 +0530

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -275,15 +275,15 @@ async def get_readable_message(
             )
             msg += (
                 f"\n{get_progress_bar_string(progress)} » <b><i>{progress}</i></b>"
-                f"\n<code>Status :</code> <b>{tstatus}</b>"
-                f"\n<code>Done   :</code> {task.processed_bytes()} of {task.size()}"
-                f"\n<code>Speed  :</code> {task.speed()}"
-                f"\n<code>ETA    :</code> {task.eta()}"
-                f"\n<code>Past   :</code> {elapsed}"
-                f"\n<code>User   :</code> <b>{user_tag}</b>"
-                f"\n<code>UserID :</code> ||{task.listener.user_id}||"
-                f"\n<code>Upload :</code> {task.listener.mode}"
-                f"\n<code>Engine :</code> <b><i>{task.engine}</i></b>"
+                f"\n<code>•Status :</code> <b>{tstatus}</b>"
+                f"\n<code>•Done   :</code> {task.processed_bytes()} of {task.size()}"
+                f"\n<code>•Speed  :</code> {task.speed()}"
+                f"\n<code>•ETA    :</code> {task.eta()}"
+                f"\n<code>•Past   :</code> {elapsed}"
+                f"\n<code>•User   :</code> <b>{user_tag}</b>"
+                f"\n<code>•UserID :</code> ||{task.listener.user_id}||"
+                f"\n<code>•Upload :</code> {task.listener.mode}"
+                f"\n<code>•Engine :</code> <b><i>{task.engine}</i></b>"
             )
             if hasattr(
                 task,

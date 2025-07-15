2eea2e44ea97c374b9371dce0ed2fe14e83b428c
Finn <haryanvivibe0077@gmail.com>
2025-07-15 12:32:59 +0530

    Update token_manager.py

--- bot/helper/ext_utils/token_manager.py
+++ bot/helper/ext_utils/token_manager.py
@@ -202,7 +202,7 @@ async def start(client, message):
     ):
         start_string = "Bot Started.\n" \
                        "Now I will send all of your stuffs here.\n" \
-                       "Use me at: @Nxleech"
+                       "Use me at: @NxMirror"
     elif (
         not config_dict["DM_MODE"]
         and message.chat.type != message.chat.type.SUPERGROUP
